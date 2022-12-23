a6d4398aa1d3169c1964d925da5177a7cbe91ab3
Denise Ratasich <dratasich@users.noreply.github.com>
2022-12-23 07:38:00 +0100

    Add ocr with tesseract on jpg

---
+++ digitize.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# %%
+import argparse
+import glob
+import os
+
+import pytesseract
+from PIL import Image
+
+# %%
+desc = "Digitize scans and pdfs - convert images and pdfs to text files."
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument(
+    "path",
+    default=".",
+    type=str,
+    help=f"""Directory of files to digitize.""",
+)
+args = parser.parse_args()
+
+# %% get inputs for OCR (images in directory)
+files = glob.glob(f"{args.path}/*.jpg")
+
+# %% load images
+for f in files:
+    try:
+        image = Image.open(f)
+        print(f"File '{f}': {image.format_description} {image.mode} {image.info}")
+        print(f"Run tesseract on {f}...")
+        text = pytesseract.image_to_string(image, lang="eng+deu")
+        outputfile = f"{os.path.splitext(f)[0]}_ocr.txt"
+        with open(outputfile, "w") as o:
+            o.write(text)
+        print(f"Wrote {outputfile}")
+    except:
+        print(f"Failes to digitize {f}")
