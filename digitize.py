81643847f9c59112a2498d837b04aaf422fa7e1e
Denise Ratasich <dratasich@users.noreply.github.com>
2022-12-23 08:41:00 +0100

    Add pdf to text conversion

--- digitize.py
+++ digitize.py
@@ -5,7 +5,9 @@ import argparse
 import glob
 import os
 
+import PyPDF2
 import pytesseract
+from pdf2image import convert_from_path
 from PIL import Image
 
 # %%
@@ -29,7 +31,7 @@ parser.add_argument(
     "--extension",
     type=str,
     nargs="+",
-    default=["jpg", "png"],
+    default=["jpg", "png", "pdf"],
     help=f"""Extensions of files to search for.""",
 )
 args = parser.parse_args()
@@ -39,16 +41,40 @@ files = []
 for ext in args.extension:
     files.extend(glob.glob(f"{args.path}/**/*.{ext}", recursive=args.recursive))
 
-# %% load images
+# %% save text:
+def save(text: str, to: str) -> bool:
+    with open(to, "w") as o:
+        o.write(text)
+    print(f"Wrote {to}")
+
+
+# %% convert pdf
+def pdf2text(path: str) -> str:
+    print(f"Run pdf reader on {f}...")
+    reader = PyPDF2.PdfReader(f)
+    print(
+        f"File '{f}': {len(reader.pages)} page(s) {reader.metadata} {reader.pdf_header}"
+    )
+    text = "".join(reader.pages[0].extract_text())
+    if len(text) < 5:
+        # no text in pdf -> convert to image and try with ocr
+        doc = convert_from_path(path)
+        text = "".join([pytesseract.image_to_string(page) for page in doc])
+    return text
+
+
+# %% convert all files
 for f in files:
     try:
-        image = Image.open(f)
-        print(f"File '{f}': {image.format_description} {image.mode} {image.info}")
-        print(f"Run tesseract on {f}...")
-        text = pytesseract.image_to_string(image, lang="eng+deu")
         outputfile = f"{os.path.splitext(f)[0]}_ocr.txt"
-        with open(outputfile, "w") as o:
-            o.write(text)
-        print(f"Wrote {outputfile}")
-    except:
-        print(f"Failes to digitize {f}")
+        if str(f).endswith(".pdf"):
+            text = pdf2text(f)
+        else:
+            print(f"Run tesseract on {f}...")
+            image = Image.open(f)
+            print(f"File '{f}': {image.format_description} {image.mode} {image.info}")
+            text = pytesseract.image_to_string(image, lang="eng+deu")
+        if len(text) > 5:
+            save(text, outputfile)
+    except Exception as e:
+        print(f"Failed to digitize {f}: {e}")
